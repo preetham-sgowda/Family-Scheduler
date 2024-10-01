529d7ee5a9048a82f132c60a606595756ccc2501
Preetham S Gowda <sgowdapreetham14@gmail.com>
2024-10-01 19:31:02 +0530

    Add files via upload

---
+++ Home/home.js
@@ -0,0 +1,11 @@
+document.getElementById('Calender').addEventListener('click', function() {
+    window.location.href = '/calender/cal.html'; // Redirect to the calendar page
+});
+
+document.getElementById('Family-Tree').addEventListener('click', function() {
+    window.location.href = '/tree/tree.html'; // Redirect to the family tree page
+});
+
+document.getElementById('Account').addEventListener('click', function() {
+    window.location.href = '/log/log.html'; // Redirect to the login page
+});
